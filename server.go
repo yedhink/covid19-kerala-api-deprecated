f7cd0a9102236118d55392ade47686f42db91d8d
yedhink <yedhin1998@gmail.com>
2020-03-31 22:36:07 +0530

    GET the latest file title and link to page with english button

--- server.go
+++ server.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/anaskhan96/soup"
 )
 
+func ScrapeDHSKerala() (string) {
+	resp, err := soup.Get("http://dhs.kerala.gov.in/%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	doc := soup.HTMLParse(resp)
+	links := doc.Find("h3", "class", "entry-title").Find("a")
+	return strings.ReplaceAll(links.Text(),"/","-")+".pdf"
+}
+
 func main() {
-	fmt.Println()
+	fmt.Println(ScrapeDHSKerala())
 }

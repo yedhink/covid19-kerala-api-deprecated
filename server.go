c68b1824a2a5b7b7220f6386864a2cca562f10b7
yedhink <yedhin1998@gmail.com>
2020-04-01 12:19:47 +0530

    finish retrieval of the link of the latest pdf file and rename the method into GetLatestPDF

--- server.go
+++ server.go
@@ -11,17 +11,18 @@ import (
 
 type Scraper interface {
 	GetMainPage(chan [2]string)
+	GetLatestPDF(chan string)
 }
 
 type Website struct {
-	baseURL         string
-	mainPageURL     string
-	latestFileURL   string
-	bulletinPageURL string
+	baseURL          string
+	mainPageURL      string
+	bulletinPageURL  string
+	latestPDFFileURL string
 }
 
-func Scrape(w Website) (soup.Root) {
-	resp, err := soup.Get(w.baseURL + w.mainPageURL)
+func Scrape(base string, route string) soup.Root {
+	resp, err := soup.Get(base + route)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -29,8 +30,16 @@ func Scrape(w Website) (soup.Root) {
 	return soup.HTMLParse(resp)
 }
 
-func (w Website) GetMainPage(c chan [2]string) {
-	doc := Scrape(w)
+func (w *Website) GetLatestPDF(c chan string) {
+	doc := Scrape(w.baseURL, w.bulletinPageURL)
+	fmt.Printf("bullet url : %s\n",w.bulletinPageURL)
+	links := doc.Find("div", "class", "entry-content").Find("a")
+	w.latestPDFFileURL = links.Attrs()["href"]
+	c <- w.baseURL + w.latestPDFFileURL
+}
+
+func (w *Website) GetMainPage(c chan [2]string) {
+	doc := Scrape(w.baseURL, w.mainPageURL)
 	links := doc.Find("h3", "class", "entry-title").Find("a")
 	latestFileName := "data/" + strings.ReplaceAll(links.Text(), "/", "-") + ".pdf"
 	bulletinPage := links.Attrs()["href"]
@@ -38,11 +47,11 @@ func (w Website) GetMainPage(c chan [2]string) {
 }
 
 func main() {
-	var website = Website{
+	var website = &Website{
 		baseURL: "http://dhs.kerala.gov.in/",
-		mainPageURL: `%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-`+
-						`%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1`+
-						`%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/`,
+		mainPageURL: `%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-` +
+			`%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1` +
+			`%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/`,
 	}
 	var sc Scraper = website
 
@@ -56,15 +65,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	c := make(chan [2]string)
+	chan1 := make(chan [2]string)
+	chan2 := make(chan string)
 	for _, f := range files {
-		go sc.GetMainPage(c)
-		x := <-c
+		go sc.GetMainPage(chan1)
+		x := <-chan1
 		website.bulletinPageURL = x[1]
 		if f == x[0] {
 			fmt.Println("The pdf file is already latest")
 		} else {
 			fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n", f, x[0])
+			go sc.GetLatestPDF(chan2)
+			fmt.Printf("lastest file : %s\n",<-chan2)
 		}
 	}
 }

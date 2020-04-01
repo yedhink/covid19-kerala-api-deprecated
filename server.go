b277b5858e4c55c994a71b510df4d436081b9998
yedhink <yedhin1998@gmail.com>
2020-04-01 10:27:25 +0530

    add structuring by creating a scraper interface and structure to hold site details

--- server.go
+++ server.go
@@ -9,33 +9,56 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-func ScrapeDHSKerala(c chan string) {
-	resp, err := soup.Get("http://dhs.kerala.gov.in/%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/")
+type Scraper interface {
+	GetMainPage(chan [2]string)
+}
+
+type Website struct {
+	baseURL string
+	mainPageURL string
+	latestFileURL string
+	bulletinPageURL string
+}
+
+func (w Website) GetMainPage(c chan [2]string) {
+	resp, err := soup.Get(w.baseURL+w.mainPageURL)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	doc := soup.HTMLParse(resp)
 	links := doc.Find("h3", "class", "entry-title").Find("a")
-	c<-"data/"+strings.ReplaceAll(links.Text(),"/","-")+".pdf"
+	latestFileName := "data/"+strings.ReplaceAll(links.Text(),"/","-")+".pdf"
+	bulletinPage :=links.Attrs()["href"]
+	c<-[2]string{latestFileName,bulletinPage}
 }
 
 func main() {
+	var website = Website{
+		baseURL: "http://dhs.kerala.gov.in/",
+		mainPageURL: "%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/",
+	}
+	var sc Scraper = website
+
+	/*
+	Glob ignores file system errors such as I/O errors reading directories.
+	The only possible returned error is ErrBadPattern, when pattern is malformed.
+	*/
 	files, err := filepath.Glob("data/*.pdf")
     if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
     }
 
-	c := make(chan string)
+	c := make(chan [2]string)
     for _, f := range files {
-			go ScrapeDHSKerala(c)
+			go sc.GetMainPage(c)
 			x := <-c
-			if f == x {
+			website.bulletinPageURL = x[1]
+			if f == x[0] {
 				fmt.Println("The pdf file is already latest")
 			} else {
-				fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n",f,x)
+				fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n",f,x[0])
 			}
     }
-	// fmt.Println(ScrapeDHSKerala())
 }

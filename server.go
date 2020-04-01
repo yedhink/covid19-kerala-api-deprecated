f2e834b72acbd20c2f8bf0b5372f41715a969a4f
yedhink <yedhin1998@gmail.com>
2020-04-01 10:32:40 +0530

    split long main page url into mutltiline concatanations

--- server.go
+++ server.go
@@ -14,51 +14,53 @@ type Scraper interface {
 }
 
 type Website struct {
-	baseURL string
-	mainPageURL string
-	latestFileURL string
+	baseURL         string
+	mainPageURL     string
+	latestFileURL   string
 	bulletinPageURL string
 }
 
 func (w Website) GetMainPage(c chan [2]string) {
-	resp, err := soup.Get(w.baseURL+w.mainPageURL)
+	resp, err := soup.Get(w.baseURL + w.mainPageURL)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	doc := soup.HTMLParse(resp)
 	links := doc.Find("h3", "class", "entry-title").Find("a")
-	latestFileName := "data/"+strings.ReplaceAll(links.Text(),"/","-")+".pdf"
-	bulletinPage :=links.Attrs()["href"]
-	c<-[2]string{latestFileName,bulletinPage}
+	latestFileName := "data/" + strings.ReplaceAll(links.Text(), "/", "-") + ".pdf"
+	bulletinPage := links.Attrs()["href"]
+	c <- [2]string{latestFileName, bulletinPage}
 }
 
 func main() {
 	var website = Website{
 		baseURL: "http://dhs.kerala.gov.in/",
-		mainPageURL: "%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/",
+		mainPageURL: `%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-`+
+						`%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1`+
+						`%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/`,
 	}
 	var sc Scraper = website
 
 	/*
-	Glob ignores file system errors such as I/O errors reading directories.
-	The only possible returned error is ErrBadPattern, when pattern is malformed.
+		Glob ignores file system errors such as I/O errors reading directories.
+		The only possible returned error is ErrBadPattern, when pattern is malformed.
 	*/
 	files, err := filepath.Glob("data/*.pdf")
-    if err != nil {
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-    }
+	}
 
 	c := make(chan [2]string)
-    for _, f := range files {
-			go sc.GetMainPage(c)
-			x := <-c
-			website.bulletinPageURL = x[1]
-			if f == x[0] {
-				fmt.Println("The pdf file is already latest")
-			} else {
-				fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n",f,x[0])
-			}
-    }
+	for _, f := range files {
+		go sc.GetMainPage(c)
+		x := <-c
+		website.bulletinPageURL = x[1]
+		if f == x[0] {
+			fmt.Println("The pdf file is already latest")
+		} else {
+			fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n", f, x[0])
+		}
+	}
 }

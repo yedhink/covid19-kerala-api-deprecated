0e194c6cb454cdee8ada24e52873aff5a9d3f154
yedhink <yedhin1998@gmail.com>
2020-04-01 23:18:06 +0530

    add method to delete local pdf

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,18 +7,32 @@ import (
 )
 
 type Storage struct {
-	Path string
+	BasePath string
+	LocalFilePath string
+	RemoteFileName string
 }
 
-func (s Storage) LocalPDFName() []string{
+
+func (s Storage) Download() {
+	var err = os.Remove(s.LocalFilePath)
+	if err != nil {
+		fmt.Printf("os.remove error\t: was not able to delete %s\nEXITING....\n",s.LocalFilePath)
+		os.Exit(0)
+	} else {
+		fmt.Printf("Succesfully removed the local pdf %s\n",s.LocalFilePath)
+	}
+}
+
+func (s *Storage) LocalPDFName() []string{
 	/*
 		Glob ignores file system errors such as I/O errors reading directories.
 		The only possible returned error is ErrBadPattern, when pattern is malformed.
 	*/
-	files, err := filepath.Glob(s.Path+"*.pdf")
+	files, err := filepath.Glob(s.BasePath+"*.pdf")
 	if err != nil || len(files) == 0{
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	s.LocalFilePath = files[0]
 	return files
 }

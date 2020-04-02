317845f25684c482eacf23ffce9d9c7347a09dcb
yedhink <yedhin1998@gmail.com>
2020-04-02 16:06:02 +0530

    init and start a test scheduler

--- cmd/covid19keralaapi/server.go
+++ cmd/covid19keralaapi/server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	. "github.com/yedhink/covid19-kerala-api/internal/scheduler"
-	"github.com/robfig/cron/v3"
 )
 
 
 func main() {
-	c := cron.New()
-	c.AddFunc("* 15-16 * * *", BackgroundDaemon)
-	c.Run()
-	select {}
+	sc := Scheduler{
+		Spec : "* 16-20 * * *",
+	}
+	sc.Schedule()
 }

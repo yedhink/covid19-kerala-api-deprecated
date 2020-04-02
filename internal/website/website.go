e32d46879d25cea86bd6b59c7d3bfa739d371b12
yedhink <yedhin1998@gmail.com>
2020-04-02 10:30:19 +0530

    :tada:complete the method to download remote pdf and save it to local storage

--- internal/website/website.go
+++ internal/website/website.go
@@ -1,7 +1,10 @@
 package website
 
 import (
+	"fmt"
+	"io/ioutil"
 	"strings"
+	"net/http"
 	. "github.com/yedhink/covid19-kerala-api/internal/scraper"
 	storage "github.com/yedhink/covid19-kerala-api/internal/storage"
 )
@@ -25,4 +28,23 @@ func (w *Website) GetMainPage() ([2]string){
 	bulletinPage := links.Attrs()["href"]
 	return [2]string{latestFileName, bulletinPage}
 }
+
+func (w *Website) Download(st *storage.Storage) error{
 	st.Delete()
+	resp,err := http.Get(w.BaseURL+w.LatestPDFURL)
+	if err != nil {
+		return HttpError{err,"http : client policy error or connectivity problems"}
+	} else if resp.StatusCode >= 404{
+		return HttpError{resp.Status,"http error : failed to GET pdf from "+w.BaseURL+w.LatestPDFURL}
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Trouble reading reesponse body!")
+	}
+	err = ioutil.WriteFile(st.BasePath+st.RemoteFileName, contents, 0644)
+	if err != nil {
+		fmt.Printf("Trouble creating file : %v\n", err)
+	}
+	return nil
+}

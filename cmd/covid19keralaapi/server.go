deda005b6d29bcd176b472dee2c6a3a5be11e40b
yedhink <yedhin1998@gmail.com>
2020-04-02 15:53:42 +0530

    move the scheduler functions into separate pkg and invoke cron to run from 18-20pm every 5 mins

--- cmd/covid19keralaapi/server.go
+++ cmd/covid19keralaapi/server.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-
-	. "github.com/yedhink/covid19-kerala-api/internal/scraper"
-	. "github.com/yedhink/covid19-kerala-api/internal/storage"
-	. "github.com/yedhink/covid19-kerala-api/internal/website"
+	. "github.com/yedhink/covid19-kerala-api/internal/scheduler"
+	"github.com/robfig/cron/v3"
 )
 
+
 func main() {
-	var website = &Website{
-		BaseURL: "http://dhs.kerala.gov.in",
-		MainPageURL: `/%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-` +
-			`%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1` +
-			`%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/`,
-	}
-	var sc Scraper = website
-	var st = &Storage{
-		BasePath : "data/",
-	}
-
-	file := st.LocalPDFName()
-
-	res := sc.GetMainPage()
-	website.BulletinPageURL = res[1]
-	if file == st.BasePath+res[0] {
-		fmt.Println("The pdf file is already latest")
-	} else {
-		st.RemoteFileName = res[0]
-		fmt.Printf("You need latest pdf file : %s(local) != %s(remote)\n", file, st.BasePath+res[0])
-		fmt.Printf("lastest file : %s\n",sc.GetLatestPDF())
-		err := website.Download(st)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			// hardcoded since the scripts dir under pipenv
-			cmd := exec.Command("python3", "scripts/extract-text-data.py","-w")
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Printf("%s", out)
-		}
-	}
+	c := cron.New()
+	c.AddFunc("* 15-16 * * *", BackgroundDaemon)
+	c.Run()
+	select {}
 }

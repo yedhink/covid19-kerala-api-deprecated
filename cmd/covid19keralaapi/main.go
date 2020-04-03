1cf5fbb1ced5b50810d274a8ad12ed144ac53d1c
yedhink <yedhin1998@gmail.com>
2020-04-03 21:57:09 +0530

    move objects of different internal pkgs into main for sharing realtime data and reusability

--- cmd/covid19keralaapi/main.go
+++ cmd/covid19keralaapi/main.go
@@ -7,8 +7,20 @@ import (
 
 
 func main() {
-	sc := Scheduler{
-		Spec : "* * * * *",
+	var website = &Website{
+		BaseURL: "http://dhs.kerala.gov.in",
+		MainPageURL: `/%e0%b4%a1%e0%b5%86%e0%b4%af%e0%b4%bf%e0%b4%b2%e0%b4%bf-` +
+			`%e0%b4%ac%e0%b5%81%e0%b4%b3%e0%b5%8d%e0%b4%b3%e0%b4%b1` +
+			`%e0%b5%8d%e0%b4%b1%e0%b4%bf%e0%b4%a8%e0%b5%8d%e2%80%8d/`,
+	}
+
+	var scraper Scraper = website
+
+	var storage = &Storage{
+		BasePath : "data/",
+		JsonFileName: "data.json",
+		LocalFileExist: false,
+	}
 	}
 	go sc.Schedule()
 	server.Start()

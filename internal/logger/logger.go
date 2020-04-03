580a3a29ae6b1ea03c3c07a46e95d64ff55efb18
yedhink <yedhin1998@gmail.com>
2020-04-03 15:48:34 +0530

    change the log file name to make it solely for scheduler logs

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -28,7 +28,10 @@ func Success(format string,v ...interface{}) string{
 
 
 func init() {
-	fileLog, err := os.OpenFile("/tmp/covid19-kerala-api.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// open the file in readwrite mode | create it if not exist | append to end of file | perm : 0666
+	// currently the color ascci code is also appended as its to log file
+	// I havent figured out how to disable it on a io.Multiwriter
+	fileLog, err := os.OpenFile("/tmp/covid19-kerala-api-scheduler.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Error opening file:", err)
 	}

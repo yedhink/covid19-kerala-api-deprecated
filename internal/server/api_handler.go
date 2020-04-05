83112492b93dac381455f1bfbbe868528d8d0fd4
yedhink <yedhin1998@gmail.com>
2020-04-05 20:49:03 +0530

    :lipstick:improve the output by making it intended json

--- internal/server/api_handler.go
+++ internal/server/api_handler.go
@@ -3,6 +3,6 @@ import "github.com/gin-gonic/gin"
 
 func (server *Server) Api() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, server.JsonData.All.Data)
+		c.IndentedJSON(200, server.JsonData.All.Data)
 	}
 }

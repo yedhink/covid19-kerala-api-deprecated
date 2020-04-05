de5f0d15fde9f944d3ccf8fb729455a4a1c215e8
yedhink <yedhin1998@gmail.com>
2020-04-05 17:59:30 +0530

    :alembic:refactor the model with a usable dataset which contains a Data and Timeline, with constructor

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,20 @@
 package model
 
+type DataSet struct{
+    TimeData TimeLine
+    All Data
+}
+
+type TimeLine struct {
+    TimeLine map[string]interface{}
+}
+
+func NewTimeLine() TimeLine{
+    var t TimeLine
+    t.TimeLine = make(map[string]interface{})
+    return t
+}
+
 type Data struct {
     Data map[string]interface{}
 }

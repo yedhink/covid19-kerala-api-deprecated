90efdd5e5e93631bb222dba987820dda39223c63
yedhink <yedhin1998@gmail.com>
2020-04-08 08:19:52 +0530

    :tada:create separate module for table data extraction and filtering

---
+++ scripts/utils/filters.py
@@ -0,0 +1,43 @@
+import re
+
+
+def extract_date(data):
+    for line in data.split('\n'):
+        if line.strip().startswith("Date") or line.strip().startswith("DATE"):
+            return re.findall(r'(\d+)/(\d+)/(\d+)', line.strip())
+    print("Wasn't able to extract Date from pdf")
+    return ""
+
+
+def extract_annex1(data):
+    table = []
+    flag = False
+    for line in data.split('\n'):
+        line = line.strip()
+        if re.match(r'^on today', line) is not None:
+            flag = True
+            continue
+        if flag and re.match(
+                r'^Thir|^Koll|^Path|^Iduk|^Kott|^Alap|^Erna|^Thri|^Pala|^Mala|^Kozh|^Waya|^Kann|^Kasa',
+                line):
+            table.append(line)
+        elif flag and re.match(r'^[T|t]otal', line):
+            table.append(line)
+            return table
+    return []
+
+
+def extract_district(data):
+    table = []
+    captured = re.findall(
+        r'District(?:\s+)(?:[N|n]o\..*?[\n\r])(.*Total.*?\n)', data, re.DOTALL)
+    for line in captured[0].split('\n'):
+        line = line.strip()
+        if re.match(
+                r'^Thir|^Koll|^Path|^Iduk|^Kott|^Alap|^Erna|^Thri|^Pala|^Mala|^Kozh|^Waya|^Kann|^Kasa|^\d+\s',
+                line):
+            table.append(line)
+        elif re.match(r'^[T|t]otal', line):
+            table.append(line)
+            return table
+    return table

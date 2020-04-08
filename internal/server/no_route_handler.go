ba33c988df5f786bc2be7af78d37091a5ad59bd5
yedhink <yedhin1998@gmail.com>
2020-04-08 23:58:10 +0530

    :pencil2:fix #5 : add handler for invalid/no routes and fix typo in err msg

--- internal/server/no_route_handler.go
+++ internal/server/no_route_handler.go
@@ -4,6 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func (server *Server) NoRouteErr() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.IndentedJSON(404, gin.H{"success": false, "message": "Page not found"})
+		c.IndentedJSON(404, gin.H{"success": false, "message": "Page Not Found"})
 	}
 }

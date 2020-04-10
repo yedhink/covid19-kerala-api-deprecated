a2b45a7b13235f655b74f37616622f4e6a23d286
yedhink <yedhin1998@gmail.com>
2020-04-10 10:14:29 +0530

    :art:format the code for consistency across all contributors

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,39 +1,39 @@
 package model
 
-type DataSet struct{
-    TimeLineData TimeLine
-    All Data
-    Districts Location
+type DataSet struct {
+	TimeLineData TimeLine
+	All          Data
+	Districts    Location
 }
 
 type TimeLine struct {
-    TimeLine map[string]interface{}
+	TimeLine map[string]interface{}
 }
 
-type Location struct{
-    Loc map[string]interface{}
+type Location struct {
+	Loc map[string]interface{}
 }
 
 type Data struct {
-    Data map[string]interface{}
+	Data map[string]interface{}
 }
 
 func NewData() Data {
-    var d Data
-    d.Data = make(map[string]interface{})
-	d.Data["success"]=false
-    d.Data = make(map[string]interface{})
-    return d
+	var d Data
+	d.Data = make(map[string]interface{})
+	d.Data["success"] = false
+	d.Data = make(map[string]interface{})
+	return d
 }
 
-func NewTimeLine() TimeLine{
-    var t TimeLine
-    t.TimeLine = make(map[string]interface{})
-    return t
+func NewTimeLine() TimeLine {
+	var t TimeLine
+	t.TimeLine = make(map[string]interface{})
+	return t
 }
 
 func NewLocation() Location {
-    var l Location
-    l.Loc = make(map[string]interface{})
-    return l
+	var l Location
+	l.Loc = make(map[string]interface{})
+	return l
 }
